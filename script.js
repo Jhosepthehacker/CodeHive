606f36720c0a531d8a036c02b8c34d16b1a05ad1
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-05-10 15:09:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let mensaje = "Mi página web HTML"
-let name = "Code Hive"
-console.log (name)
-alert (name)
-console.log (mensaje)
-alert (mensaje)
-label for="name" Escribe tu nombre 
-input type="text" placeholder="Escribe tu nombre" name="nombre"
+let nombre = "Hola ";
+let nombre2 = "A Todos"
+alert ("¡Hola, " + nombre + nombre2 + "!");
