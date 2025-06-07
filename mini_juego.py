7900cb20e9126e90ecd6e0d4563321e653b453b6
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-06-06 21:16:13 -0400

    Update mini_juego.py

--- mini_juego.py
+++ mini_juego.py
@@ -2,20 +2,20 @@ import random as r
 import time as t
 import sys
 
-def juego():
-    name = input("¡Hola usuario!, ¿Cómo te llamas?: ")
-    print("")
-    pregunta = input(f"{name}, ¿Quiéres jugar?: ")
-    if pregunta.lower() == "si" or pregunta.lower() == "sí":
+def juego():   # Se declara la función juego
+    name = input("¡Hola usuario!, ¿Cómo te llamas?: ")  # Entrada de datos "input"
+    print("")    # Salto de línea 
+    pregunta = input(f"{name}, ¿Quiéres jugar?: ") # Entrada de datos "input" si el usuario quiere jugar o no
+    if pregunta.lower() == "si" or pregunta.lower() == "sí": # Comprobando la respuesta del usuario
         options_game = ["######################################",
                         "#                                     ",
-                        "# adivinanzas | adventura, | combate  ",
+                        "# adivinanzas | adventura, | combate  ",  # Opciones de juego
                         "#                                     ",
                         "######################################",
                        ]
-        for i in options_game:
-            print(i)
-            print("")
+        for i in options_game:  # Iterando la lista "options_game"
+            print(i)  # Imprimiendo el valor de la variable "i" que almacenó la iteración de la lista "options_game"
+            print("")  # Salto de línea
         type_game = input(f"{name}, ¿Qué tipo de juego prefieres?: ")
         if type_game.lower() == "adivinanzas" or type_game.lower() == "adivinar":
             numero = r.randint(1, 100)
@@ -35,3 +35,8 @@ def juego():
         elif pregunta.lower() == "no":
             print("Operación cancelada")
             sys.exit()
+
+# Iniciar el juego llamando la función "juego"
+
+if __name__ == '__main__':
+    juego()
