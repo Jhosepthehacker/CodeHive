a489d4bf3d035a6fa46bc3d021bb923dab59b67c
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-06-06 21:05:55 -0400

    Update mini_juego.py

--- mini_juego.py
+++ mini_juego.py
@@ -1,3 +1,4 @@
+import random as r
 import time as t
 
 def juego():
@@ -11,4 +12,22 @@ def juego():
                         "#                                     ",
                         "######################################",
                        ]
-        type_game = input(f"{name},¿Qué tipo de juego prefieres?: ")
+        for i in options_game:
+            print(i)
+            print("")
+        type_game = input(f"{name}, ¿Qué tipo de juego prefieres?: ")
+        if type_game.lower() == "adivinanzas" or type_game.lower() == "adivinar":
+            numero = r.randint(1, 100)
+            intentos = 0
+            while True:
+                try:
+                    entrada = int(input("Adivina un número del 1 al 100"))
+                except ValueError:
+                    print("¡Respuesta Inválida!")
+                if entrada > numero:
+                    print("Demasiado alto")
+                elif entrada < numero:
+                    print("Demasiado bajo")
+                elif entrada == numero:
+                    print(f"Felicidades has encontrado el número {numero} en {intentos} intentos")
+                    break
