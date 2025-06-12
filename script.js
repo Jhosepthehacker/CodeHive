339a7981c7a24cc3a5e90d0929569736b6c6e2a0
thegoodhacker96 <gloriamariasulbaran0@gmail.com>
2025-06-12 18:25:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ if (saludos == "Bienvenidos"){
         alert("¡Hola, " + saludos + saludos2 + "!");
         console.log("¡Hola, " + saludos + saludos2 + "!");
 };
-funtion frontend(){
+function frontend(){
         let container.innerHTML = document.body;
         container.innerHTML += `<p id="definición">Hay dos tipos de programación, front-end y back-end.</p>`
 };
