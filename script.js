eb38c57737f9073384d74033ee8b1653f4ef13ae
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-06-12 10:52:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let saludos = "Bienvenidos ";
 let saludos2 = "A Mi Página Web";
-if (saludos == "Bienvenidos");{
+if (saludos == "Bienvenidos"){
         alert("¡Hola, " + saludos + saludos2 + "!");
         console.log("¡Hola, " + saludos + saludos2 + "!");
-}
+};
+funtion frontend(){
+        container.innerHTML = "<p id="definición">Hay dos tipos de programación, front-end y back-end.</p>"
+};
