2e2ed0c19ecc14bec17579a60ebd969c0db76e59
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-06-14 14:42:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const saludos2 = "A Mi Página Web";
 if (saludos == "Bienvenidos"){
         alert("¡Hola, " + saludos + saludos2 + "!");
         console.log("¡Hola, " + saludos + saludos2 + "!");
-};
+}
 //Mostrar información
 function tipos(){
         document.getElementById('miContenido').style.display = 'block';
-};
+}
 //Ocultar información
 function ocultarTipos(){
         document.getElementById('miContenido').style.display = 'none';
