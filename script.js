8cac81b6d999903c1b907d119ed93526bc8b9433
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-06-16 09:58:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let saludos = "Bienvenidos ";
-const saludos2 = "A Mi Página Web";
-if (saludos == "Bienvenidos"){
-        alert("¡Hola, " + saludos + saludos2 + "!");
-        console.log("¡Hola, " + saludos + saludos2 + "!");
+let saludos = "Bienvenidos "; // Declarar variable
+const saludos2 = "A Mi Página Web"; // Declarar Constante
+if (saludos == "Bienvenidos"){  // Comprobando valores y condiciones de la varible saludos
+        alert("¡Hola, " + saludos + saludos2 + "!");  // Mostrar alerta en la página web
+        console.log("¡Hola, " + saludos + saludos2 + "!"); // Imprimir en la consola
 }
 //Mostrar información
 function tipos(){
