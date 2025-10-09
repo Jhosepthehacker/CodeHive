489cab29d3b9c6b49dd55474e4854b334101ae24
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-10-09 09:15:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ import {Random} from "random-js";
 
 const Random = new Random();
 
+while (true){
 const colors = [
         "#0000FF", "#FF0000", "#8888FF",
         "#24DA68", "#8888FF", "#42AD86"
 ];
 
-const number_random = null;
+const color_random = Random.choice(colors);
+
+document.getElementById('').style.color = `${color_random}`;
+}
 
 let saludos = "Bienvenidos "; // Declarar variable
 const saludos2 = "A Mi Página Web"; // Declarar Constante
+
 if (saludos == "Bienvenidos"){  // Comprobando valores y condiciones de la varible saludos
         alert("¡Hola, " + saludos + saludos2 + "!");  // Mostrar alerta en la página web
         console.log("¡Hola, " + saludos + saludos2 + "!"); // Imprimir en la consola
