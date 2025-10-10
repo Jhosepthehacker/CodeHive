f9b01a22f95d1e846510df119d6593675f55e95b
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-10-09 23:08:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,11 @@ import { Random } from "random-js";
 
 const random = new Random();
 
-function colorRandom(){
-  const colors = [
-        "#0000FF", "#FF0000", "#8888FF",
-        "#24DA68", "#8888FF", "#42AD86"
-];
+function message(){
+  const welcomeMessage = "Hola Bienvenido(a)";
 
-  const color = random.pick(colors);
-
-  document.getElementById('mostrarInforme').style.color = color;
-  document.getElementById('saludando').style.color = `${color_random}`;
+  alert(welcomeMessage);
+  console.log(welcomeMessage);
 }
 
-colorRandom();
+message();
