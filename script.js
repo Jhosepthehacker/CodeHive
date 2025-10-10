175b6f54861d997b4d47388f4dc3613abed46f2d
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-10-09 21:36:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,26 +12,6 @@ function colorRandom(){
 
   document.getElementById('mostrarInforme').style.color = color;
   document.getElementById('saludando').style.color = `${color_random}`;
-
-  return true;
 }
 
-const saludos = "Bienvenidos "; // Declarar variable
-const saludos2 = "A Mi Página Web"; // Declarar Constante
-
-if (saludos === "Bienvenidos"){  // Comprobando valores y condiciones de la varible saludos
-        alert("¡Hola, " + saludos + saludos2 + "!");  // Mostrar alerta en la página web
-        console.log("¡Hola, " + saludos + saludos2 + "!"); // Imprimir en la consola
-}
-// Mostrar información
-function tipos(){
-        document.getElementById('mostrarInfo').style.display = 'block';
-        const result = colorRandom();
-        alert(result);
-}
-// Ocultar información
-function ocultarTipos(){
-        document.getElementById('mostrarInfo').style.display = 'none';
-}
-
-// En desarrollo....
+colorRandom();
