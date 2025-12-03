d1d1916974a9c4c2bada340468961aff91a1f8a7
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-12-02 22:01:24 -0400

    Implement API request handling in app.js
    Added function to send requests and handle API response.

---
+++ app.js
@@ -0,0 +1,38 @@
+const sendRequests = () => {
+  async function getData() {
+    // Prueba de API en localhost
+    const API_URL = "http://127.0.0.1:8080/message";
+
+    const response = await fetch(API_URL, {
+      method: 'GET'
+    });
+
+    try {
+      if (!response.ok) {
+        throw new Error(`Ha ocurrido un error: ${response.ok}`);
+      }
+
+      const json = await response.json();
+      const message = json.message;
+
+      alert("Response Of The Backend: " + message);
+      console.log("Response Of The Backend: " + message);
+    }
+   catch (error) {
+     console.log(`Ha ocurrido un error: ${response.ok}`);
+    }
+  }
+}
+
+const message = () => {
+  const welcomeMessage = "Hola Bienvenido(a)";
+
+  alert(welcomeMessage);
+  console.log(welcomeMessage);
+}
+
+const seeEmoji = () => {
+  document.getElementById('emojiHappy').style.display = "block";
+}
+
+message();
